61662486db8aa04ccd5e19057e9276bd3acc0605
MoranChery <chery@post.bgu.ac.il>
2020-05-18 17:48:15 +0300

    done log in

--- app.js
+++ app.js
@@ -1,46 +1,61 @@
 var server = 'http://localhost:9000/';
 
-function signIn() {
-    $.ajax({
-        //todo - change the ip
-        url: server,
-        type: 'GET',
-        contentType: 'application/json',
-        success: function(data){
-            alert(data.hi);
-        },
-        error: function(xhr){
-            var divBlock= document.getElementById("SignIn");
-            var divNone = document.getElementById("Welcome");
-            changeToDiv(divBlock,divNone);
-            alert("Sign In");
-            //alert(xhr.responseJSON.message);
+function getLogInDiv(){
+
+    // Get the modal
+    var modal = document.getElementById("LogInDiv");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    $(document).keydown(function(event) {
+        if (event.keyCode == 27) {
+            $('#LogInDiv').hide();
         }
     });
 }
 
 function logIn() {
-    var teamName = "{teamName}";
+    var userEma = document.getElementById("user").value;
+    var pass = document.getElementById("pass").value;
     $.ajax({
         //todo - change the ip
-        url: server+'teams/' + teamName,
+        url: server+'login/{'+userEma+'}/{'+pass+'}/',
         type: 'GET',
         contentType: 'application/json',
         success: function(data){
-            alert(data.TeamName);
+            var screen = data.UserType+"Screen.html";
+            window.location=screen;
         },
         error: function(xhr){
-            var divBlock= document.getElementById("LogIn");
-            var divNone = document.getElementById("Welcome");
-            changeToDiv(divBlock,divNone);
-            //alert("Log In");
+                alert("Something got wrong");
+            //  alert(xhr.message);
+             alert(xhr.responseJSON.message);
 
-            alert(xhr.responseJSON.message);
         }
     });
 }
 
-function getUserScreen() {
+function getAlertOfUser() {
     var moran = "moran";
     $.ajax({
         //todo - change the ip
@@ -54,8 +69,8 @@ function getUserScreen() {
             var divBlock= document.getElementById("LogIn");
             var divNone = document.getElementById("Welcome");
             changeToDiv(divBlock,divNone);
-            alert("Log In");
-            //alert(xhr.responseJSON.message);
+            //alert("llog in");
+            alert(xhr.responseJSON.status);
         }
     });
 }
